66e486980eb744430e7876f30ec1d78f42d76218
jiahao-mobi <jiahao@takemobi.com>
2025-02-07 15:29:20 -0500

    yes

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mypackage",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # Dependencies (if any)
+    ],
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A simple example package",
+    url="https://github.com/yourusername/mypackage",
+)
